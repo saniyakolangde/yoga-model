99a78c5c9ab3d574dcaadf0f0dc46c2ff5d9be3b
Saniya Kolangde <34890158+saniyakolangde@users.noreply.github.com>
2024-10-02 16:44:00 +1000

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ pose = mp_pose.Pose()
 
 app = Flask(__name__)
 # CORS(app)
-CORS(app, resources={r"/*": {"origins": ["https://localhost:44352", "https://yoga-model.onrender.com"]}})
+CORS(app, resources={r"/*": {"origins": "https://localhost:44352"}})
 
 
 # Load the trained model
@@ -105,69 +105,89 @@ def calculate_distances_v2(user_landmarks, reference_landmarks):
 
     return distances
 
-@app.route('/predict', methods=['POST'])
+
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin', 'https://localhost:44352')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+    response.headers.add('Access-Control-Allow-Credentials', 'true')
+    return response
+
+@app.route('/predict', methods=['POST', 'OPTIONS'])
 def predict_pose():
-    try:
-        data = request.json
-        # Decode the base64-encoded image
-        image_data = base64.b64decode(data['image'])
-        np_image = np.frombuffer(image_data, np.uint8)
-        frame = cv2.imdecode(np_image, cv2.IMREAD_COLOR)
+    if request.method == "OPTIONS":
+        return _build_cors_preflight_response()
+    elif request.method == "POST":
+        try:
+            data = request.json
+            # Decode the base64-encoded image
+            image_data = base64.b64decode(data['image'])
+            np_image = np.frombuffer(image_data, np.uint8)
+            frame = cv2.imdecode(np_image, cv2.IMREAD_COLOR)
 
-        lm_data = extract_landmarks(frame)
-        if lm_data is not None:
-            lm_data_reshaped = lm_data.reshape(1, -1)
-            prediction = model.predict(lm_data_reshaped)
-            class_idx = np.argmax(prediction)
-            confidence = prediction[0][class_idx]
+            lm_data = extract_landmarks(frame)
+            if lm_data is not None:
+                lm_data_reshaped = lm_data.reshape(1, -1)
+                prediction = model.predict(lm_data_reshaped)
+                class_idx = np.argmax(prediction)
+                confidence = prediction[0][class_idx]
 
-            if confidence < confidence_threshold:
-                predicted_class_name = "Unknown"
-            else:
-                predicted_class_name = class_names[class_idx]
-
-            # Return the response with prediction and feedback
-            response = {
-                'pose': predicted_class_name,
-                'confidence': float(confidence),
-                'feedback': ''
-            }
-
-            # Calculate distances and provide feedback if needed
-            reference_pose = reference_landmarks[reference_landmarks['label'] == predicted_class_name].values[:, :-1]
-            if len(reference_pose) > 0:
-                reference_landmarks_arr = np.array(reference_pose).reshape(-1, 3)
-                critical_user_landmarks = lm_data[critical_landmark_indices]
-                critical_reference_landmarks = reference_landmarks_arr[critical_landmark_indices]
-                distances = calculate_distances_v2(lm_data, reference_landmarks_arr)
-                critical_distances = distances[critical_landmark_indices]
-                cumulative_deviation = np.sum(critical_distances)
-                max_deviation_index = np.argmax(critical_distances)
-                max_deviation_value = critical_distances[max_deviation_index]
-                landmark_idx = critical_landmark_indices[max_deviation_index]
-
-                if cumulative_deviation >= cumulative_deviation_threshold:
-                    incorrect_landmarks = np.where(critical_distances >= distance_threshold)[0]
-                    feedback_texts = []
-                    # for i in incorrect_landmarks:
-                    #     landmark_idx = critical_landmark_indices[i]
-                    #     feedback_message = feedback_messages_dict.get(landmark_idx, f'Landmark {landmark_idx} is not correct.')
-                    #     feedback_texts.append(feedback_message)
-                    # response['feedback'] = " | ".join(feedback_texts)
-                    if max_deviation_value >= distance_threshold:
-                        feedback_message = feedback_messages_dict.get(landmark_idx, f'Landmark {landmark_idx} is not correct.')
-                        response['feedback'] = feedback_message
-                    else:
-                        response['feedback'] = "Amazing job! Your pose is perfect!"
+                if confidence < confidence_threshold:
+                    predicted_class_name = "Unknown"
                 else:
-                    response['feedback'] = "Great job! Your pose looks good!"
-        else:
-            response = {'pose': 'No pose detected', 'confidence': 0, 'feedback': 'No pose detected.'}
+                    predicted_class_name = class_names[class_idx]
 
-        return jsonify(response)
+                # Return the response with prediction and feedback
+                response = {
+                    'pose': predicted_class_name,
+                    'confidence': float(confidence),
+                    'feedback': ''
+                }
+
+                # Calculate distances and provide feedback if needed
+                reference_pose = reference_landmarks[reference_landmarks['label'] == predicted_class_name].values[:, :-1]
+                if len(reference_pose) > 0:
+                    reference_landmarks_arr = np.array(reference_pose).reshape(-1, 3)
+                    critical_user_landmarks = lm_data[critical_landmark_indices]
+                    critical_reference_landmarks = reference_landmarks_arr[critical_landmark_indices]
+                    distances = calculate_distances_v2(lm_data, reference_landmarks_arr)
+                    critical_distances = distances[critical_landmark_indices]
+                    cumulative_deviation = np.sum(critical_distances)
+                    max_deviation_index = np.argmax(critical_distances)
+                    max_deviation_value = critical_distances[max_deviation_index]
+                    landmark_idx = critical_landmark_indices[max_deviation_index]
+
+                    if cumulative_deviation >= cumulative_deviation_threshold:
+                        incorrect_landmarks = np.where(critical_distances >= distance_threshold)[0]
+                        feedback_texts = []
+                        # for i in incorrect_landmarks:
+                        #     landmark_idx = critical_landmark_indices[i]
+                        #     feedback_message = feedback_messages_dict.get(landmark_idx, f'Landmark {landmark_idx} is not correct.')
+                        #     feedback_texts.append(feedback_message)
+                        # response['feedback'] = " | ".join(feedback_texts)
+                        if max_deviation_value >= distance_threshold:
+                            feedback_message = feedback_messages_dict.get(landmark_idx, f'Landmark {landmark_idx} is not correct.')
+                            response['feedback'] = feedback_message
+                        else:
+                            response['feedback'] = "Amazing job! Your pose is perfect!"
+                    else:
+                        response['feedback'] = "Great job! Your pose looks good!"
+            else:
+                response = {'pose': 'No pose detected', 'confidence': 0, 'feedback': 'No pose detected.'}
+
+            return jsonify(response)
     
-    except Exception as e:
-        return jsonify({"error": str(e)})
+        except Exception as e:
+            return jsonify({"error": str(e)})
+
+def _build_cors_preflight_response():
+    response = make_response()
+    response.headers.add("Access-Control-Allow-Origin", "https://localhost:44352")
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+    response.headers.add('Access-Control-Allow-Credentials', 'true')
+    return response
 
 if __name__ == '__main__':
     app.run(debug=True)
